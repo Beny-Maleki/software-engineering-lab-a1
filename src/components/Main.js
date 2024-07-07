0b7e23289fc43438d2c49d44f700826e4bdfad2f
Benyamin Maleki <benyaminmaleki81@gmail.com>
2024-07-07 18:16:28 +0330

    Initialize front-end components

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <p>This is the main content of the website.</p>
+    </main>
+  );
+}
+
+export default MainContent;
